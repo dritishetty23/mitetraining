acb7037463731221d02e29fc1a43a81abe8fa475
dritishetty23 <4mt23ai023@mite.ac.in>
2025-02-18 10:31:55 +0530

    Create day2sol2.c

---
+++ day2sol2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main() {
+    int productCount;
+    int weight;
+
+    printf("Enter number of products:");
+    scanf("%d", &productCount);
+
+    int acceptedCount = 0;
+    int rejectedCount = 0;
+
+    for(int I = 1; I < productCount; I++) {  // Potential issue: loop starts from 1
+        printf("Enter weights (in grams):");
+        scanf("%d", &weight);
+        if(weight >= 950 && weight <= 1050) {
+            acceptedCount++;
+        } else {
+            rejectedCount++;
+        }
+    }
+printf ("Accepted products: %d\n, acceptedcount);
+printf ("Rejected products: %d\n, rejectedcount);
+    return 0;
+}
